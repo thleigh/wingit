6d31b95648cca70235fa0bab2bffc9c541fa632c
Tanner Leigh <tannerhleigh@gmail.com>
2021-02-25 16:00:03 -0800

    [components] add a components folder with the home screen and details screen components

--- App.js
+++ App.js
@@ -3,42 +3,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { View, Text, Button, SafeAreaView, StyleSheet, StatusBar, ScrollView } from "react-native";
 
-const HomeScreen = ({navigation}) => {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate("Details")}
-      />
-    </View>
-  );
-}
-
-
-const DetailsScreen = ({navigation}) => {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to the details screen again"
-        onPress={() => navigation.push("Details")}
-      /><Button
-        title="Go home"
-        onPress={() => navigation.navigate("Home")}
-      />
-      <Button
-        title="Go back"
-        onPress={() => navigation.goBack()}
-      />
-      <Button
-        title="Go to the first screen"
-        onPress={() => navigation.popToTop()}
-      />
-    </View>
-  );
-}
-
+import HomeScreen from "./components/HomeScreen";
+import DetailsScreen from "./components/DetailsScreen";
 
 const Stack = createStackNavigator();
 
@@ -55,7 +21,7 @@ const App = () => {
         }
       }}>
         <Stack.Screen name="Home" component={HomeScreen} options={{
-          
+          title: "Overview"
         }}/>
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
