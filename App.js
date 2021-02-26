93ead041099bbb07254a8f58f4ef57b4a7a7d003
Tanner Leigh <tannerhleigh@gmail.com>
2021-02-25 16:11:50 -0800

    [app.js] implement drawer screen

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
 import { View, Text, Button, SafeAreaView, StyleSheet, StatusBar, ScrollView } from "react-native";
 
 import HomeScreen from "./components/HomeScreen";
 import DetailsScreen from "./components/DetailsScreen";
 
-const Stack = createStackNavigator();
+const HomeStack = createStackNavigator();
+const DetailsStack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{
+const HomeStackScreen = ({ navigation }) => (
+      <HomeStack.Navigator screenOptions={{
         headerStyle: { 
           backgroundColor: "#009387"
         },
@@ -20,13 +22,35 @@ const App = () => {
           fontWeight: "bold"
         }
       }}>
-        <Stack.Screen name="Home" component={HomeScreen} options={{
+        <HomeStack.Screen name="Home" component={HomeScreen} options={{
           title: "Overview"
         }}/>
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
+      </HomeStack.Navigator>
+);
+
+const DetailsStackScreen = ({ navigation }) => (
+      <DetailsStack.Navigator screenOptions={{
+        headerStyle: { 
+          backgroundColor: "#009387"
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold"
+        }
+      }}>
+        <DetailsStack.Screen name="Details" component={DetailsScreen} />
+      </DetailsStack.Navigator>
+);
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeStackScreen} />
+        <Drawer.Screen name="Details" component={DetailsStackScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 export default App;
