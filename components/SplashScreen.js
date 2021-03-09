6160da575b72210cc9b75712f0b80b9977e1df80
Tanner Leigh <tannerhleigh@gmail.com>
2021-03-08 23:11:12 -0800

    [splashscreen.js] add header and footer

--- components/SplashScreen.js
+++ components/SplashScreen.js
@@ -10,11 +10,12 @@ import {
 const SplashScreen = () => {
     return (
         <View style={styles.container}>
-            <Text>SplashScreen</Text>
-            <Button 
-                title="Click Here"
-                onPress={() => alert("Button Clicked!")}
-            />
+            <View styles = {styles.header}>
+                <Text>Header</Text>
+            </View>
+            <View style={styles.footer}>
+                <Text>Footer</Text>
+            </View>
         </View>
     )
 }
