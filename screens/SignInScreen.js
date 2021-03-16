73f65d579825827652f24a2cf8bc145dfb37f9fe
Tanner Leigh <tannerhleigh@gmail.com>
2021-03-16 15:34:44 -0700

    [signinscreen] finish with empty sign in field aler

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -94,7 +94,15 @@ const SignInScreen = ({navigation}) => {
         const foundUser = Users.filter( item => {
             return userName == item.username && password == item.password
         });
-        if (foundUser.length == 0 ) {
+
+        if ( data.username.length == 0 || data.password.length == 0 ) {
+            Alert.alert("Wrong Input!", "Username or password cannot be empty.", [
+                {text: "Okay"}
+            ]);
+            return;
+        };
+
+        if ( foundUser.length == 0 ) {
             Alert.alert("Invalid User", "Username or password is incorect.", [
                 {text: "Okay"}
             ]);
