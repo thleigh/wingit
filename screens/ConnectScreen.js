94674aeaec4f7b5eb1d1c5b0cc76f7af4d147214
Tanner Leigh <tannerhleigh@gmail.com>
2021-04-08 23:26:58 -0700

    [connectscreen] containertop

--- screens/ConnectScreen.js
+++ screens/ConnectScreen.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
+import { block } from 'react-native-reanimated';
 
 const ConnectScreen = ({navigation}) => {
     return (
-      <View style={styles.container}>
-        <Image 
-          source={require('../assets/tanner2.png')}
-          style={{width : 100, height : 100}}
-        />
-        <Text style={styles.welcomeText}>Hi, I'm <Text style={{fontWeight:"bold"}}>Tanner.</Text></Text>
+      <View style={styles.screen}>
+        <View style={styles.containerTop}>
+          <Image 
+            source={require('../assets/tanner2.png')}
+            style={{width : 100, height : 100}}
+          />
+          <View style={{marginLeft:30}}>
+            <Text style={styles.welcomeText}>Hi, I'm <Text style={{fontWeight:"bold"}}>Tanner.</Text></Text>
+            <Text>I need to find a new housemate @ Berkeley</Text>
+          </View>
+        </View>
       </View>
     );
 };
@@ -16,10 +22,18 @@ const ConnectScreen = ({navigation}) => {
 export default ConnectScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1, 
-    alignItems: 'center', 
-    justifyContent: 'center'
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerTop: {
+    flexDirection:'row',
+    width: 300,
+    height: 200,
+    alignItems: 'stretch', 
+    justifyContent: 'center',
+    marginTop: 0,
   },
   welcomeText: {
     fontSize: 25
