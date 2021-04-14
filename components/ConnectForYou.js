a900f165bef3ae372f4dfd0636d69a93c11bd8f8
Tanner Leigh <tannerhleigh@gmail.com>
2021-04-13 20:04:57 -0700

    [connectforyou] add map to connect component

--- components/ConnectForYou.js
+++ components/ConnectForYou.js
@@ -1,19 +1,37 @@
 import * as React from "react";
-import { View, Text, Button, StyleSheet, Image } from 'react-native';
+import { View, Text, Button, StyleSheet, Image } from "react-native";
+import datas from "../data.js";  
 
 const ConnectForYouScreen = ({navigation}) => {
-    return (
-      <View style={styles.container}>
-        <Text>Connect For You Screen</Text>
-        <View>
-          <Image 
-            source={require("../assets/tanner2.png")}
-            style={[{width : 100, height : 100}]}
-          />
+  const array = [
+    {
+      key: '1',
+      title: 'example title 1',
+      subtitle: 'example subtitle 1',
+    },
+    {
+      key: '2',
+      title: 'example title 2',
+      subtitle: 'example subtitle 2',
+    },
+    {
+      key: '3',
+      title: 'example title 3',
+      subtitle: 'example subtitle 3',
+    },
+  ];
+  const list = () => {
+    return Array.map((element) => {
+      return (
+        <View key={element.key} style={styles.container}>
+          <Text>{element.title}</Text>
+          <Text>{element.subtitle}</Text>
         </View>
-      </View>
-    );
-};
+      );
+    });
+  };
+  return <View>{list()}</View>
+}
 export default ConnectForYouScreen;
 
 const styles = StyleSheet.create({
