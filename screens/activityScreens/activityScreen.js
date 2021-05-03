a07ae68d2a9bdfaa9fcac68f293ef86e25f15fab
Tanner Leigh <tannerhleigh@gmail.com>
2021-05-02 20:36:55 -0700

    [activityscreen] finish

--- screens/activityScreens/activityScreen.js
+++ screens/activityScreens/activityScreen.js
@@ -4,28 +4,59 @@ import { StyleSheet, Text, View, Button, Alert } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import Icon from 'react-native-vector-icons/Ionicons';
 
-const ActivityScreen = () => {
+const ActivityScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
       <View style={styles.rowOne}>
         <TouchableOpacity style={[{backgroundColor: "lightgreen"}, styles.gridBox]}
-          onPress={()=> {}}
+          onPress={()=> navigation.goBack()}
         >
           <View style={styles.boxOne}>
-            <Icon name="bicycle-outline" size={70} color="white"/>
+            <Icon name="bicycle-outline" size={40} color="white"/>
             <Text style={styles.boxText}>Activity</Text>
           </View>
         </TouchableOpacity>
+
+        <View style={{marginTop: 30}}>
+          <TouchableOpacity style={[styles.subBox, styles.gridBox]}
+            onPress={()=> {}}
+          >
+            <View style={styles.boxOne}>
+              <Text style={styles.subBoxText}>Surfing</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.subBox, styles.gridBox]}
+            onPress={()=> {}}
+          >
+            <View style={styles.boxOne}>
+              <Text style={styles.subBoxText}>Snowboarding</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.subBox, styles.gridBox]}
+            onPress={()=> {}}
+          >
+            <View style={styles.boxOne}>
+              <Text style={styles.subBoxText}>Weightlifting</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.subBox, styles.gridBox]}
+            onPress={()=> {}}
+          >
+            <View style={styles.boxOne}>
+              <Text style={styles.subBoxText}>Biking</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
 };
 
-
-
 const styles = StyleSheet.create({
   container: {
-    marginTop: -20
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
   headerText: {
     textAlign: "center",
@@ -35,20 +66,31 @@ const styles = StyleSheet.create({
   boxText: {
     color: "white",
     fontSize: 25,
+    marginLeft: 25,
   },
+  subBoxText: {
+    color: "black",
+    fontSize: 25,
+  },
+  subBox: {
+    borderWidth: 1,
+    borderColor: "lightgreen",
+    marginTop: 2,
+  }, 
   gridBox: {
     margin: 20,
     borderRadius: 8,
-    width: 160, 
-    height: 180,
+    width: 300, 
+    height: 70,
     justifyContent: "center",
   },
   boxOne: {
     // flex: 1,
+    flexDirection: "row",
     justifyContent:"center",
     alignItems:"center",
     textAlignVertical: "center",
-    padding: 20,
+    padding: 10,
     color: "#fff",
   },
 });
