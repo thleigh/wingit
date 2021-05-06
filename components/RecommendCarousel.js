c87ebcc835614eb05fa7c6cde545864611c059b6
Tanner Leigh <tannerhleigh@gmail.com>
2021-05-05 20:07:49 -0700

    [recommendCarousel] set up callbacks and usestate for the index and slide size

--- components/RecommendCarousel.js
+++ components/RecommendCarousel.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { FlatList } from "react-native-gesture-handler";
+import { useState } from "react";
 
 const { width: windowWidth, heigth: windowHeight} = Dimensions.get("window");
 
@@ -35,6 +36,25 @@ function slide({data}) {
 }
 
 function Carousel() {
+    const [index, setIndex] = useState(0);
+    const indexRef = useRef(index);
+    indexRef.current = index;
+    const onScroll = useCallback((event) => {
+        const slideSize = event.nativeEvent.layoutMeasurement.width;
+        const index = event.nativeEvent.contentOffset.x / slideSize;
+        const roundIndex = Math.round(index);
+
+        const distance = Math.abs(roundIndex - index);
+
+        // Prevent one pixel triggering setIndex in the middle of the transition.
+        // With this we have to scroll a bit more to trigger the index change.
+        const isNoMandsLand = 0.4 < distance;
+
+        if (roundIndex !== indexRef.current && !isNoMandsLand) {
+            setIndex(roundIndex);
+        }
+    }, []);
+
     return (
         <FlatList
             data={slideList}
@@ -42,6 +62,11 @@ function Carousel() {
             renderItem={({item}) => {
                 return <Slide data={item}/>
             }}
+            pagingEnabledhorizontal
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            onScroll={onScroll}
+
         />
     )
 }
